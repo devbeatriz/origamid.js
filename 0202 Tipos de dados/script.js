04125b9d99ee7047de6e7a01fa5c172846a35bdb
github_devbeatriz <beatrizrodrrigues@gmail.com>
2022-10-24 15:05:02 -0300

    tipos de dados

---
+++ 0202 Tipos de dados/script.js
@@ -0,0 +1,19 @@
+// Declare uma variável contendo uma string
+var nome = 'Beatriz';
+
+// Declare uma variável contendo um número dentro de uma string
+var anoNascimento = '2000';
+
+// Declare uma variável com a sua idade
+var idade = 22;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var sobrenome = 'Rodrigues';
+console.log(nome + ' ' + sobrenome);
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = 'It\'s time';
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
