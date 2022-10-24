ef1c20538fb0e1eedab46c7e47c3ff64e101750f
github_devbeatriz <beatrizrodrrigues@gmail.com>
2022-10-24 14:29:34 -0300

    variaveis

---
+++ 0201 Variáveis/script.js
@@ -0,0 +1,20 @@
+// Declarar uma variável com o seu nome
+var nome = 'Bea';
+
+// Declarar uma variável com a sua idade
+var idade = '22'
+
+//Declarar uma variável com a sua comida favorita e não atribuir valor
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = 'Pizza';
+
+// Declarar 5 variáveis diferentes sem valores
+var time, 
+    professor, 
+    pais, 
+    endereco, 
+    rua;
+
+console.log();
